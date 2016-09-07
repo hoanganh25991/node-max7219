aa49a29e720cf461423cfd4cab47366eb85e9a65
Anh Le Hoang <lehoanganh25991@gmail.com>
2016-09-07 15:22:55 +0700

    basic npm-package ^^

---
+++ index.js
@@ -0,0 +1,107 @@
+var max7219 = function(options){
+	var originOptions = {
+		message: 'hello world',
+		cascade: 1,
+		brightness: 1,
+		vertical: true
+	};
+	/**
+	 * default options
+	 */
+	var defaultOptions = {
+		message: 'hello world',
+		cascade: 1,
+		brightness: 1,
+		vertical: true
+	};
+	/**
+	 * in next version
+	 */
+	// var defaultOptions = {};
+	// Object.keys(originOptions).forEach(function(key){
+	// 	defaultOptions[key] = originOptions[key];
+	// });
+
+	//push options into default {}
+	//when use not submit
+	//in node 6x, we can set default for params
+	options = options ? options : {};
+
+	// //Object.keys return empty array []
+	// //if options is NOT an object or $%^
+	// Object.keys(options).forEach(function (key) {
+	// 	//let user-options override default
+	// 	defaultOptions[key] = options[key];
+	// });
+	/**
+	 * by define setOptions for use easy custommize
+	 * initialize options by call setOptions
+	 */
+
+	/**
+	 * store exec process
+	 * to terminate it when run a new one
+	 */
+
+	/**
+	 * execute python command
+	 */
+	var exec = require('child_process').exec;
+
+	var drawText = function(){
+		console.log('drawText based on', options);
+		// var cmd = 'sudo python ./drawText';
+		console.log(__dirname);
+		var cmd = 'sudo python ' + __dirname + '/bin/drawText.py';
+		console.log(cmd);
+		exec(cmd, function(err, stdout){
+			console.log(stdout);
+		});
+	};
+
+	var getOptions = function(){
+		return options;
+	};
+
+	var getDefaultOption = function(){
+		//like an original options
+		//we ALWAYS return this one
+		//instead of modified defaultOptions
+		return {
+			message: 'hello world',
+			cascade: 1,
+			brightness: 1,
+			vertical: true
+		};
+	};
+
+	var setOptions = function(opt){
+		//Object.keys return empty array []
+		//if options is NOT an object or $%^
+		Object.keys(opt).forEach(function(key){
+			//let user-options override default
+			defaultOptions[key] = opt[key];
+		});
+		//assign back to options
+		options = defaultOptions;
+	};
+
+	/**
+	 * initialize needded object
+	 * @return {[type]} [description]
+	 */
+	var init = function(){
+		setOptions(options);
+	};
+
+	init();
+
+	return {
+		drawText: drawText,
+		getOptions: getOptions,
+		setOptions: setOptions,
+		getDefaultOption: getDefaultOption
+	};
+};
+
+module.exports = max7219;
---
+++ test/basic.js
@@ -0,0 +1,7 @@
+var Max7219 = require('../index.js');
+
+var max7219 = Max7219();
+
+// console.log(max7219.getOptions());
+
+max7219.drawText();
